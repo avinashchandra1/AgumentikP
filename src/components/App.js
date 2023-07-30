4cf061e8773be4406904032f1ff6a8eb452ca32f
avinash chandra <avinash.009835@gmail.com>
2023-07-30 22:05:26 +0530

    Agumentic

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroBanner from './HeroBanner';
+import ContentSection from './ContentSection';
+
+function App() {
+  return (
+    <div className="app">
+      <HeroBanner />
+      <ContentSection />
+    </div>
+  );
+}
+
+export default App
